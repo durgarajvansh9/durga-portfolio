0827327adcf1fe64b947b34ea744f5318b4193a4
Rk <durga@alobhadev.com>
2024-09-18 19:11:24 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+  "use strict";
+
+  // COLOR MODE
+  $(document).ready(function () {
+    // Set the default state to dark mode
+    $('body').addClass('dark-mode'); // Add dark mode by default
+    $('.color-mode-icon').addClass('active'); // Set the icon to active for dark mode
+
+    // Toggle dark mode on click
+    $('.color-mode').click(function () {
+      $('.color-mode-icon').toggleClass('active');
+      $('body').toggleClass('dark-mode');
+    });
+  });
+
+
+  // HEADER
+  $(".navbar").headroom();
+
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true
+  });
+
+  // SMOOTHSCROLL
+  $(function () {
+    $('.nav-link, .custom-btn-link').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
+
+  // TOOLTIP
+  $('.social-links a').tooltip();
+
+})(jQuery);
